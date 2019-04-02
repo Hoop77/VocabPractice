62006902bb6fb7128bd27511e5c820a823a77d0a
Hoop77 <p.badenhoop@gmx.de>
2019-04-02 12:54:33 +0200

    alphabetically

--- practice.py
+++ practice.py
@@ -9,7 +9,7 @@ def read_words(f):
             words.append(word)
     return words
 
-def practice(words_ger, words_eng, eng_to_ger=True):
+def practice_random_subset(words_ger, words_eng, eng_to_ger=True):
     num_exercise_words = int(input('Number of words to exercise: '))
     num_words = len(words_ger)
     indices = np.array(list(range(num_words)))
@@ -26,10 +26,23 @@ def practice(words_ger, words_eng, eng_to_ger=True):
             input('\t{}'.format(word_ger))
             print('\t{}'.format(word_eng))
 
+def practice_alphabetically(words_ger, words_eng, eng_to_ger=True):
+    num_words = len(words_ger)
+    key = lambda k: words_eng[k] if eng_to_ger else lambda k: words_ger[k]
+    indices = sorted(range(num_words), key=key)
+    for i in range(num_words):
+        print('{}/{}'.format(i + 1, num_words))
+        if eng_to_ger:
+            input('\t{}'.format(words_eng[indices[i]]))
+            print('\t{}'.format(words_ger[indices[i]]))
+        else:
+            input('\t{}'.format(words_ger[indices[i]]))
+            print('\t{}'.format(words_eng[indices[i]]))
+
 if __name__ == '__main__':
     with open('german.txt', 'r', encoding='utf8') as file_ger:
         with open('english.txt', 'r') as file_eng:
             words_ger = read_words(file_ger)
             words_eng = read_words(file_eng)
-            practice(words_ger, words_eng)
+            practice_alphabetically(words_ger, words_eng)
             
